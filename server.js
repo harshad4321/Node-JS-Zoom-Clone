ea38c08f0b2fe193b74ca552d3d6e23457f90e32
Harshad N <103301901+harshad4321@users.noreply.github.com>
2022-07-08 12:55:21 +0530

    uuid added

--- server.js
+++ server.js
@@ -1,9 +1,14 @@
 const express = require('express')
 const app = express();
-const server = require('http').server(app);
+const server = require('http').Server(app);
+const { v4: uuidv4 } = require('uuid');
+app.set('view engine','ejs');
 
 
 
+app.get('/:room',(req,res)=>{
+    res.render("room", {roomId: req.params.room })
+})
 
 
 
